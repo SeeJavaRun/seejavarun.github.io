d167bcc85c81c9c5b402b651044bf42ba12af4ea
Dan Langford <danlangford@gmail.com>
2020-09-10 01:25:38 -0600

    favico and other polish

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: __dirname,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: ['css-loader'],
+      },
+    ]
+  }
+};
